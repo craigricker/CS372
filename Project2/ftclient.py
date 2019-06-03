3c4057c58d9b97bafe7a6fbff2014e81a8e7dd94
craigricker <craigd.ricker@gmail.com>
2019-06-02 21:37:21 -0700

    Test on flip

--- Project2/ftclient.py
+++ Project2/ftclient.py
@@ -1,126 +1,135 @@
-#!/bin/python
-import argparse
+"""
+    ProgramName:        ftclient
+    Author:             Craig Ricker
+    Course Name:        CS372 Intro to Computer Networks
+    Last Modified:      June 2nd, 2019
+    Description:
+
+    This is the cilent end of the ftclient. It creates a connection using command line
+    arguments to an up and running server. It then interacts with the server, and the
+    user to either download a specific file, or see the contents of the current directory
+    Beej's guide was very helpful, as was my Project 1 for CS372
+
+"""
 import sys
-import socket
+from socket import *
+import os
+import random
 
-GLOBAL_TERM = "**"
-
-def send_message(user_name, conn):
-    """
-    Using connection created by socket API, prompts user for a message, and then sends it
-    through the connection.
-    Pre conditions: user_name must be valid text and conn must be active two way socket powered connection
-    Post conditions: Message is sent, if user types \quit it will return false, otherwise true
-    :param user_name:   Username to be included for prompt and sent message
-    :param conn:        Connection to send message on
-    :return:            T/F if the user wants to continue the chat conversation
-    """
-    # Use username to prompt and include in sent message
-    to_send = user_name + "> " + input(user_name + "> ")
-    # Must encode string to be sent, need simple data type
-    conn.sendall(to_send.encode())
-    if '\quit' in to_send:
-        return False
-    else:
-        return True
-
-
-def receive_messsage(conn):
-    """
-    Through connection created by socket API, receives messages on passed connection (conn) and then displays
-    to the user.
-    If the message contains "\quit" return False, other wise return true.
-    Pre conditions: conn must be active two way socket powered connection
-    Post conditions: Message is sent, if user types \quit it will return false, otherwise true
-    Used thi
-    :param conn:    Connection to receive message from
-    :return:        T/F if other user wishes to continue conversation
-    """
-    data = conn.recv(1024)
-    # Decode message so appears as normal text
-    data = data.decode('ascii')
-    if '\quit' in data:
-        return False
-    else:
-        print(data)
-        return True
-
-# https://stackoverflow.com/questions/48812854/python3-sending-files-via-socket
-def receive_file(conn, file_name):
-    # Open file_name to write to
-    writer = open(file_name, 'wb')
-    data = conn.recv(1024)
-    # Read until end of file
-    while data != bytes("".encode()):
-        writer.write(data)
-        data.recv(1024)
-
-
-
-
-
-def listen_socket(port):
-    """
-    Starts listening for connection on specificed port number
-    This code is adapted from socket API documentation
-    Pre conditions: port must be a valid port number that is not currently in use
-    Post condition: initates two way connection to port and returns connection
-    :param port:    port to listen for connection
-    :return:        created socket
-    """
-    HOST = ''  # Symbolic name meaning all available interfaces
-    my_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_socket.bind((HOST, port))
-    my_socket.listen(1)
-    conn, addr = my_socket.accept()
-    return conn
-
-def connect_server(host, port):
-    to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    to_server.connect((host, port))
-    return to_server
-
-
-def broadcast_socket(host, port):
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_socket.connect((host, port))
-    return my_socket
 
 def my_ip():
     """
-    # http://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
-    :return:
+    # https://www.geeksforgeeks.org/python-program-find-ip-address/
+    Was helpful in writing this program...I'm not sure how we are
+    supposed to read if it is flip1 or flip2 so I am going with iP
+    :return: ip address of current system
     """
-    s = socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+    hostname = gethostname()
+    return gethostbyname(hostname)
 
 
-def ftp_me(data_port, single_file=True):
-    client_socket = broadcast_socket(client_port)
-    # Tell client which port to send file on
-    client_socket.send(data_port)
-    # Send current IP address as well
-    client_socket.send()
+def establish_connection(host, port):
+    """
+    Creates a socket, but an outgoing socket
+    :param host: host name to connect to
+    :param port: port to connect to
+    :return: newly created socket
+    """
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((host, port))
+    return client_socket
 
-    # Either send command to list files, or send specific file
-    if single_file:
-        # Request single file
-        # Check if file exists
-        response = receive_messsage(conn)
-        if response != "good":
-            print(response)
+
+def create_socket(port):
+    """
+    Function to setup a socket, and wait until a connection comes in
+    :param port: port to create a socket to listen to
+    :return: newly created socket
+    """
+    client_port = int(port)
+    sockfd = socket(AF_INET, SOCK_STREAM)
+    sockfd.bind(('', client_port))
+    sockfd.listen(0)
+    data_socket, address = sockfd.accept()
+    return data_socket
+
+
+def recv_all(the_socket):
+    """
+    Repeatedly reads data until connection is broken
+    Adopted from # http://code.activestate.com/recipes/408859/
+    :param the_socket: socket to read data from
+    :return: all data combined into a single str
+    """
+    total_data=[]
+    while True:
+        data = the_socket.recv(8192)
+        if not data: break
+        total_data.append(data.decode())
+    return ''.join(total_data)
+
+
+def recv_all_tofile(the_socket, filename):
+    """
+    Adopted from # http://code.activestate.com/recipes/408859/
+    writes to file, if filename exists already adds random integers
+    :param the_socket: socket to read from
+    :param filename: file you are requesting
+    :return: none
+    """
+    total_data=[]
+    while os.path.isfile(filename):
+        filename = filename + str(random.randint(1,101))
+    writer = open(filename, 'w')
+    while True:
+        data = the_socket.recv(8192)
+        if not data: break
+        writer.write(data.decode())
+
+
+
+
+def talkToServer(client_socket, data_port, command, file_name, host):
+    """
+    Sends over identifying information to create socket
+    Allows server to validate commands, and then executes commands
+    :param client_socket: socket to reach server on
+    :param data_port: port to have data come back
+    :param command: command that is being executed
+    :param file_name: if used, file being requested
+    :param host: host to connect to
+    :return: None
+    """
+    # Send port number
+    client_socket.send(data_port.encode())
+    client_socket.recv(1024).decode()
+    # Host
+    client_socket.send(my_ip().encode())
+    client_socket.recv(1024).decode()
+    # Command
+    client_socket.send(command.encode())
+    if (client_socket.recv(1024).decode() == "invalid"):
+        print("You have entered an invalid command, exiting")
+        exit(1)
+
+    # If requesting file, send file name
+    if command == "-g":
+        # Send file name
+        client_socket.send(file_name.encode())
+        response = client_socket.recv(1024).decode()
+        if "invalid" in response:
+            print("{}:{} says FILE NOT FOUND".format(host, data_port))
             exit(1)
-        else:
-            receive_file(conn)
-
+        data_socket = create_socket(data_port)
+        print('Receiving "{}" from {}:{}'.format(file_name, host, data_port))
+        # Receive and write to file
+        recv_all_tofile(data_socket, file_name)
+        print("File transfer complete.")
     else:
-        # request list of files to chose from
-        print(receive_messsage(conn))
-
+        print("Receiving directory structure from {}:{}.".format(host, data_port))
+        data_socket = create_socket(data_port)
+        print(recv_all(data_socket))
     client_socket.close()
-
-
 
 
 if __name__ == "__main__":
@@ -141,12 +150,15 @@ if __name__ == "__main__":
         print("Port must be within range of [10,000 - 65,535]")
         exit(1)
     else:
-        host = sys.argv[1]#+".engr.oregonstate.edu"
-        host = 'localhost'
+        host = sys.argv[1]
+        # Add eng info if using flip
+        if host == "flip1" or host == "flip2":
+            host = ".engr.oregonstate.edu"
         port = int(sys.argv[2])
-        to_server = connect_server(host, port)
-
-
-
-
-
+        # Data port is last one, uses trick
+        data_port = sys.argv[-1]
+        command = sys.argv[3]
+        # If -l is used, this is garbage, but never used
+        file_name = sys.argv[4]
+        client_socket = establish_connection(host, port)
+        talkToServer(client_socket, data_port, command, file_name, host)
---
+++ Project2/test folder/ftserver.c
@@ -0,0 +1,314 @@
+/*
+ ProgramName:        ftserver.c
+ Author:             Craig Ricker
+ Course Name:        CS372 Intro to Computer Networks
+ Last Modified:      June 2nd, 2019
+ Description:
+ 
+ This acts as a server which can send it's directory's contents, or a
+ single file over a socket. It starts on a port provided by the command lien
+ and listens for an incoming connection. It then reads in the information
+ required to create a socket, command, and potentially a file.
+ 
+ It then executes the command, while validating the file if required
+ 
+ The methodology of connecting to sockets was learned/borrowed  from
+ Beej's guide to networking - most specifically the setup_address
+ and socket_setup functions in this file.
+ 
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define MAX_CONNS 5
+/*
+ * Quick & dirty error print + quit
+ * Pre condition: None
+ * Inputs: code, error code to return, error message to print
+ * Returns: none
+ * Post Conditions: program exits immediatly
+ */
+void error(int code, const char *error) {
+   fprintf(stderr, "%s\n", error);
+   exit(code);
+}
+
+/*
+ * Creates a connection to the socket
+ * Pre condition: input has been validated and is working
+ * Inputs:  addr, address to connect to
+ *          port: port to connect to
+ * Returns: new connection
+ * Post Conditions:
+ */
+struct addrinfo *createConnection(char *addr, char *port) {
+   struct addrinfo hints;
+   struct addrinfo *res;
+   int status = 0;
+   memset(&hints, 0, sizeof hints);
+   // Setup for TCP style connection
+   hints.ai_family = AF_INET;
+   hints.ai_socktype = SOCK_STREAM;
+   if ((status = getaddrinfo(addr, port, &hints, &res)) != 0) {
+      error(2, "Error establishing connection.\n");
+   }
+   return res;
+}
+
+/*
+ * Creates a socket to conn
+ * Pre condition: conn is valid
+ * Inputs: conn a valid struct
+ * Returns: newly created file descriptor
+ * Post Conditions: file descriptor is added
+ */
+int createSocket(struct addrinfo *conn) {
+   int sockfd;
+   if ((sockfd = socket(conn->ai_family, conn->ai_socktype, conn->ai_protocol)) == -1) {
+      error(-1, "Failed to create socket.\n");
+   }
+   
+   return sockfd;
+}
+
+/*
+ * Connects so socket
+ * Pre condition: input valid
+ * Inputs:  sockfd: file descriptor
+ *          res info attached
+ * Returns: None
+ * Post Conditions: connection is connected
+ */
+void connectSocket(int sockfd, struct addrinfo * res){
+   int status;
+   if ((status = connect(sockfd, res->ai_addr, res->ai_addrlen)) == -1){
+      error(1, "Error in connection.\n");
+   }
+}
+
+/*
+ * Binds socket
+ * Pre condition:
+ * Inputs: file descriptor and a connection
+ * Returns: None
+ * Post Conditions: socket is bound
+ */
+void bindSocket(int sockfd, struct addrinfo *conn) {
+   if (bind(sockfd, conn->ai_addr, conn->ai_addrlen) == -1) {
+      close(sockfd);
+      error(1, "Could not bind socket.\n");
+   }
+}
+
+/*
+ * Listens to socket for connection
+ * Pre condition: socfd is established and ready to listen
+ * Inputs: working fd
+ * Returns: none
+ * Post Conditions: Socket waits for connection
+ */
+void listenSocket(int sockfd) {
+   if (listen(sockfd, MAX_CONNS) == -1) {
+      close(sockfd);
+      error(1, "Could not listen to socket.\n");
+   }
+}
+
+/*
+ * Goes through the current directory and sends a list of all
+ * the files to the connection
+ * The below link was incredibly helpful for understanding how
+ * to navigate files in C
+ * https://www.sanfoundry.com/c-program-list-files-directory/
+ * The files .. and . are special files, and are ignored
+ * Pre condition: connection is active
+ * Inputs: connection - where to send the file names
+ * Returns: none
+ * Post Conditions: file names are sent
+ */
+void list_files(int connection) {
+// https://www.sanfoundry.com/c-program-list-files-directory/
+   DIR * d;
+   struct dirent * dir;
+   char buffer[100] = "";
+   d = opendir(".");
+   if (d)
+   {
+      // Look at every single file
+      while ((dir = readdir(d)) != NULL) {
+         memset(&buffer, 0, sizeof buffer);
+         strcpy(buffer, dir->d_name);
+         // Don't care about . or ..
+         if ((strcmp( buffer, ".") != 0) && (strcmp(buffer, "..") !=0)) {
+            strcat(buffer, " ");
+            send(connection, buffer, strlen(buffer), 0);
+         }
+      }
+      closedir(d);
+   }
+}
+
+/*
+ * Reads through file 2000 chars at a time, and sends the
+ * buffer repeatedly to connection until it is fully sent
+ * reads more of the file, and then sends that
+ * Control loop adopted from my CS344 assignment/instructors lectures
+ * Pre condition: fname has been validated as a readable file
+ * Inputs: datafd: where to send the data
+ *          fname: file to read
+ * Returns: None
+ * Post Conditions: file is transfered to datafd
+ */
+void send_file(int datafd, char *fname) {
+   char buffer[2000] = "";
+   int reader = open(fname, O_RDONLY);
+   int num_read, num_written;
+   char * scroller;
+   // Guarantees that you read to the end of the file
+   while (1) {
+      num_read = read(reader, buffer, sizeof(buffer) - 1);
+      // You've read to the end of the file
+      if (num_read == 0)
+         break;
+      // Something has gone WRONG
+      else if (num_read < 0)
+         error(1, "Failed to read file\n");
+      // Send everything that you have read so far
+      else {
+         scroller = buffer;
+         // You know how much you send, subtract from what is left
+         while (num_read > 0)
+         {
+            // Store number send
+            num_written = send(datafd, scroller, sizeof(buffer),0);
+            if (num_written < 0)
+               error(1, "Failed to write\n");
+            // Calculate what is left to send
+            num_read -= num_written;
+            // Update what you send next
+            scroller += num_written;
+         }
+         memset(buffer, 0, sizeof(buffer));
+      }
+   }
+}
+
+/*
+ * Using connection, it receives a port, an address, and a command
+ * it then executes the command given . It can either send a
+ * file over the socket that is described by the received port/address
+ * or it can send the directory contents
+ * Pre condition: connection is established
+ * Inputs:  connection: where to send data
+ * Returns: None
+ * Post Conditions:  command is executed
+ */
+void handle_connection(int connection) {
+   char port[100] = "";
+   char host[100] = "";
+   char command[100] = "";
+   char fname[100] = "";
+   char * valid = "valid";
+   char * invalid = "invalid";
+   struct addrinfo* res;
+   int datafd = 0;
+
+   // Get the port
+   recv(connection, port, sizeof(port) - 1, 0);
+   send(connection, valid, sizeof(valid), 0);
+   // Get host
+   recv(connection, host, sizeof(host) - 1, 0);
+   send(connection, valid, sizeof(valid), 0);
+   printf("Connection from %s.\n", host);
+   // Get command
+   recv(connection, command, sizeof(command) - 1, 0);
+   
+   // Ensure valid command is entered, and alert client
+   if ((strcmp(command, "-g") == 0) || (strcmp(command, "-l") == 0)) {
+      send(connection, valid, sizeof(valid), 0);
+   }
+   else {
+      printf("Invalid command!\n");
+      send(connection, invalid, sizeof(invalid), 0);
+   }
+   // Create connection to send information back
+   if (strcmp(command, "-l") == 0)
+   {
+      sleep(1);
+      printf("List directory request on port %s.\n", port);
+      printf("Sending directory contents to %s:%s\n", host, port);
+      res = createConnection(host, port);
+      datafd = createSocket(res);
+      connectSocket(datafd, res);
+      list_files(datafd);
+   }
+   else
+   {
+      // https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
+      // Used above link to detect if file exists or not
+      recv(connection, fname, sizeof(fname), 0);
+      printf("File \"%s\" requested on port %s\n", fname, port);
+      if (access( fname, F_OK ) != -1) {
+         send(connection, valid, sizeof(valid), 0);
+         printf("Sending \"%s\" on port %s\n", fname, port);
+      }
+      else {
+         printf("File not found. Sending error message.\n");
+         send(connection, invalid, sizeof(invalid), 0);
+         close(datafd);
+         return;
+      }
+      sleep(1);
+      res = createConnection(host, port);
+      datafd = createSocket(res);
+      connectSocket(datafd, res);
+      send_file(datafd, fname);
+   }
+   close(datafd);
+}
+
+/*
+ * Sits and waits for incoming connection, once connection is received
+ * hands off connection to other function
+ * Pre condition: sockfd been connected to
+ * Inputs:  sockfd connection to listen on
+ * Returns: None
+ * Post Conditions: Command is executed
+ */
+void ftclient(int sockfd) {
+   struct sockaddr_storage clientAddr;
+   socklen_t addrSize;
+   int newConnection;
+   
+   while (1) {
+      addrSize = sizeof(clientAddr);
+      printf("Waiting for connection...\n");
+      newConnection = accept(sockfd, (struct sockaddr *)&clientAddr, &addrSize);
+      handle_connection(newConnection);
+      close(newConnection);
+   }
+}
+
+int main(int argc, char *argv[]) {
+   if (argc != 2)
+      error(1, "Usage: ./ftpserver [PORTNUBER}\n");
+   int port_n = atoi(argv[1]);
+   if ((port_n >65535) || (port_n < 10000))
+      error(1, "Enter a valid port number\n");
+   printf("Server open on %s\n", argv[1]);
+   struct addrinfo* res = createConnection(NULL, argv[1]);
+   int sockfd = createSocket(res);
+   bindSocket(sockfd, res);
+   listenSocket(sockfd);
+   ftclient(sockfd);
+   freeaddrinfo(res);
+}
